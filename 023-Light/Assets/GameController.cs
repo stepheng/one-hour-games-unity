6599ad143d638b3c01fc3862d7f1b0d78b68284c
Stephen Gurnett <stephen.gurnett@gmail.com>
2017-11-16 23:04:17 +0000

    One Hour Game Jam #023 - Light
    Light is life. Stay in the light to stay alive. Test lighting,
    collisions, and player movement via the CharacterController.

---
+++ 023-Light/Assets/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	float timer = 0;
+	int currentLight = 0;
+
+	public Light[] lights;
+
+	// Use this for initialization
+	void Start () {
+		foreach(Light light in lights) {
+			light.intensity = 0;
+		}
+		lights[currentLight].intensity = 5;
+		FireTimer();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		if (timer <= 0) {
+			lights[currentLight].intensity = 0;
+			currentLight = Random.Range(0, lights.Length);
+			lights[currentLight].intensity = 5;
+			FireTimer();
+		}		
+	}
+
+	void FireTimer() {
+		timer = Random.Range(3f, 8f);
+	}
+}
---
+++ 023-Light/Assets/Player.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+	public Text healthText;	
+	CharacterController characterController;
+	float health = 100;
+
+	bool inLight = true;
+
+	// Use this for initialization
+	void Start () {
+		characterController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float speed = 0;
+		if (Input.GetKey(KeyCode.W)) {
+			speed = 10;
+		} else if (Input.GetKey(KeyCode.S)) {
+			speed = -10;
+		}
+		float hspeed = 0;
+		if (Input.GetKey(KeyCode.A)) {
+			hspeed = -10;
+		} else if (Input.GetKey(KeyCode.D)) {
+			hspeed = 10;
+		}				
+		transform.Rotate(0, Input.GetAxis("Horizontal") * 5, 0);
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+		//Vector3 strafe = transform.TransformDirection(Vector3.left) * hspeed;
+		//Vector3 merged = forward + strafe;
+        float curSpeed = speed;
+        characterController.SimpleMove(forward * curSpeed);	
+		if (inLight == false) {
+			health -= Time.deltaTime;
+			Debug.Log("Health: " + health);
+		}
+		healthText.text = "" + (int)health;
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Light light = other.gameObject.GetComponent<Light>();
+		if (light == null) { return; }
+		inLight = light.intensity > 0;
+	}
+
+	void OnTriggerStay(Collider other) {
+		Light light = other.gameObject.GetComponent<Light>();
+		if (light == null) { return; }
+		inLight = light.intensity > 0;
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (inLight && other.gameObject.tag == "Light") {
+			inLight = false;			
+		}
+	}
+}
