afe1dbf5d997cf70a8a2bb5c490f29df25d3ba8f
Stephen Gurnett <stephen.gurnett@gmail.com>
2017-11-18 21:42:42 +0000

    One Hour Game Jam #024 - Chain Reaction
    Dominos. Left-click to place, click and drag to move, right click and
    drag to rotate. Space bar to 'push' the domino under the cursor.
    
    Practicing physics, raycasting, instantiating objects, and manipulating
    them in the 3D space.

---
+++ 024-ChainReaction/Assets/Domino.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Domino : MonoBehaviour {
+
+	Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody>();		
+		rigidbody.centerOfMass = new Vector3(0, -0.5f, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {		
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			RaycastHit hit;                
+			if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
+				if (hit.transform.gameObject == this.gameObject) {
+            		rigidbody.AddForceAtPosition(new Vector3(0, 1, 0), transform.forward * 100);
+				}
+			}
+        }
+	}
+}
---
+++ 024-ChainReaction/Assets/InputClick.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputClick : MonoBehaviour {
+
+     private static Vector3 cursorWorldPosOnNCP {
+         get {
+             return Camera.main.ScreenToWorldPoint(
+                 new Vector3(Input.mousePosition.x, 
+                 Input.mousePosition.y, 
+                 Camera.main.transform.position.y - Camera.main.nearClipPlane));
+         }
+     }
+
+	private static Vector3 cameraToCursor {
+         get {
+             return cursorWorldPosOnNCP - Camera.main.transform.position;
+         }
+     }
+ 
+     private Vector3 cursorOnTransform {
+         get {
+             Vector3 camToTrans = transform.position - Camera.main.transform.position;
+             return Camera.main.transform.position + 
+                 cameraToCursor * 
+                 (Vector3.Dot(Camera.main.transform.forward, camToTrans) / Vector3.Dot(Camera.main.transform.forward, cameraToCursor));
+         }
+     }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public Domino domino;
+	private Domino selectedDomino;
+	private float yAngle = 0;
+	// Update is called once per frame
+	void Update () {		
+		if (Input.GetButtonDown("Fire1") || Input.GetButtonDown("Fire2")) {
+			RaycastHit hit;
+			if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
+				if (hit.transform.gameObject.GetComponent<Domino>() == null) {
+					Vector3 point = hit.point;
+					point.y = 1f;
+					Debug.Log(point);
+					selectedDomino = Instantiate<Domino>(domino);
+					selectedDomino.transform.position = point;
+					selectedDomino.transform.eulerAngles = new Vector3(0, yAngle, 0);
+				} else {
+					selectedDomino = hit.transform.gameObject.GetComponent<Domino>();
+				}
+			}
+		} else if (Input.GetButtonUp("Fire1") || Input.GetButtonUp("Fire2")) {
+			selectedDomino = null;			
+		}
+		if (Input.GetButtonDown("Fire2")) {
+			RaycastHit hit;
+			if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
+				selectedDomino = hit.transform.gameObject.GetComponent<Domino>();
+				if (selectedDomino != null) {
+					yAngle = selectedDomino.transform.eulerAngles.y;
+				}
+			}
+		}
+
+		if (selectedDomino != null) {
+			if (Input.GetButton("Fire2")) {
+				yAngle += Input.GetAxis("Mouse X") * 5;
+				selectedDomino.transform.eulerAngles = new Vector3(0, yAngle, 0);
+			} else if (Input.GetButton("Fire1")) {
+				Vector3 point = cursorWorldPosOnNCP;
+				point.y = 1f;
+				selectedDomino.transform.position = point;
+			}
+		}
+	}
+}
