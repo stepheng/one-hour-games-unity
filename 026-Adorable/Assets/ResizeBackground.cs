6b61d2e0a4c4b95a1c6c9600ece20ab5bcc42181
Stephen Gurnett <stephen.gurnett@gmail.com>
2017-11-23 22:24:55 +0000

    One Hour Game Jam #026 - Adorable
    Not much of a game but experimented with animations - sprite animations
    in this case - which has always been something I have had issues with.
    Did some 2D graphics, and physics, including using a capsule cast
    for determining if the player is touching the ground. Use A+D for left
    and right controls. Press S to melt. Character inspired by Putty on the
    Amiga.
    
    Added a few elements to make it look a little nicer. Blew the time limit
    but worth it just to solidify a few concepts.

---
+++ 026-Adorable/Assets/ResizeBackground.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResizeBackground : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		float width = Screen.width / 768f;
+		float height = Screen.height / 768f;
+		transform.localScale = new Vector3(width, height, 1);
+		Debug.Log("Scale: " + width + ", " + height + " - " + Screen.width + ", " + Screen.height);		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
