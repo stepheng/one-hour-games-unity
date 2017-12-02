e82ddab49e32c676d39d1116835190ab011cd407
Stephen Gurnett <stephen.gurnett@gmail.com>
2017-12-02 21:33:45 +0000

    One Hour Game Jam #029 - Bitslap's Birthday Jam
    More geometry experiments and figured out some details about lighting
    and materials.

---
+++ 029-Bitslap's Birthday Jam/Assets/CandleFlicker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandleFlicker : MonoBehaviour {
+
+	Light light;
+	public float flickerPercentage = 0.7f;
+
+	// Use this for initialization
+	void Start () {
+		light = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Random.Range(0.0f, 1.0f) > flickerPercentage) {
+			light.intensity = Random.Range(1.5f, 2.5f);
+		}
+	}
+}
