b9bbb4c1e8a1c2e958904ab5f32564cee38e816c
Stephen Gurnett <stephen.gurnett@gmail.com>
2017-12-03 00:38:15 +0000

    One Hour Game Jam #030 - Solidarity
    Navigation - have a number of agents picking random points to walk to.
    When clicking, sets a common goal for them to congregate around before
    they disperse again.

---
+++ 030-Solidarity/Assets/MouseScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseScript : MonoBehaviour {
+
+     private static Vector3 cursorWorldPosOnNCP {
+         get {
+             return Camera.main.ScreenToWorldPoint(
+                 new Vector3(Input.mousePosition.x, 
+                 Input.mousePosition.y, 
+                 Camera.main.transform.position.y - Camera.main.nearClipPlane));
+         }
+     }
+
+	private static Vector3 cameraToCursor {
+         get {
+             return cursorWorldPosOnNCP - Camera.main.transform.position;
+         }
+     }
+ 
+     private Vector3 cursorOnTransform {
+         get {
+             Vector3 camToTrans = transform.position - Camera.main.transform.position;
+             return Camera.main.transform.position + 
+                 cameraToCursor * 
+                 (Vector3.Dot(Camera.main.transform.forward, camToTrans) / Vector3.Dot(Camera.main.transform.forward, cameraToCursor));
+         }
+     }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1")) {
+			RaycastHit hit;
+			if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
+				Vector3 point = hit.point;
+				point.y = 0f;
+				transform.position = point;
+
+				NavigateScript[] navigators = FindObjectsOfType<NavigateScript>();
+				foreach (NavigateScript navigator in navigators) {
+					navigator.SetGoal(transform);
+				}
+			}
+		}	
+	}
+}
---
+++ 030-Solidarity/Assets/NavigateScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+enum NavigatorState {
+	WALKING,
+	NAVIGATING
+}
+
+public class NavigateScript : MonoBehaviour {
+
+	public Transform goal;
+	
+	private  NavMeshAgent agent;
+
+	private NavigatorState navigatorState = NavigatorState.WALKING;
+
+	private float timeToStateChange = float.MaxValue;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+		timeToStateChange = Time.realtimeSinceStartup;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch(navigatorState) {
+			case NavigatorState.NAVIGATING:
+				if (timeToStateChange <= Time.realtimeSinceStartup) {
+					navigatorState = NavigatorState.WALKING;
+					timeToStateChange = Time.realtimeSinceStartup;
+					return;
+				}
+				break;
+			case NavigatorState.WALKING:
+				if (timeToStateChange <= Time.realtimeSinceStartup) {
+					Debug.Log("Picking new destination");
+					agent.destination = new Vector3(Random.Range(-15f, 15f), 0f, Random.Range(-15f, 15f));
+					timeToStateChange = float.MaxValue;
+					return;
+				}
+				float distance = Vector3.Distance(agent.destination, transform.position);
+				Debug.Log("Distance: " + distance);
+				if (agent.isStopped || distance < 1.5f) {
+					timeToStateChange = Time.realtimeSinceStartup;					
+				}
+				break;
+		}
+	}
+
+	void FixedUpdate() {
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		timeToStateChange = Time.realtimeSinceStartup + 5f;
+	}
+
+	public void SetGoal(Transform goal) {
+		this.goal = goal;
+		agent.destination = goal.position;	
+		navigatorState = NavigatorState.NAVIGATING;
+	}
+}
